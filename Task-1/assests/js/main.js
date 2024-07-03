646c542e09e5088634909dc3d613d26af2345e0e
Muskanpopli <96069428+Muskanpopli@users.noreply.github.com>
2024-07-03 16:03:58 +0530

    Add files via upload

---
+++ Task-1/assests/js/main.js
@@ -0,0 +1,90 @@
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+    if(navToggle){
+        navToggle.addEventListener('click',()=>{
+            navMenu.classList.add('show-menu')
+        });
+    }
+
+    if(navClose){
+        navClose.addEventListener('click',()=>{
+            navMenu.classList.remove('show-menu')
+        });
+    }
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+   
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+emailjs.sendForm('service_c6eovqw','template_4u4ykac','#contact-form','Lwk9NDjggfg7T9qOz')
+    .then(()=>{
+       
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+    setTimeout(() =>{
+        contactMessage.textContent = ''
+    },5000);
+
+    contactForm.reset()
+
+    }, () =>{
+       
+         contactMessage.textContent = ' Message not sent (service error) ❌'
+    })
+}
+contactForm.addEventListener('submit', sendEmail);
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
